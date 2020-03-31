3fd89f3dff5b9bbbe96f851b66289254846f4f7c
rootsuke <rootsuke@gmail.com>
2020-03-31 22:36:22 +0900

    modify selectbox

--- frontend/todo/src/components/finish_type.vue
+++ frontend/todo/src/components/finish_type.vue
@@ -1,11 +1,19 @@
+<style lang='scss'>
+  .option-item {
+    list-style: none;
+    cursor: pointer;
+    &:hover {
+      background: azure;
+    }
+  }
+</style>
+
 <template>
   <div>
-    <select name="finish_type" v-model="local_val" @change="onChange($event.target.value)">
-      <option disabled value="">Please select one</option>
-      <option :value="1">1</option>
-      <option :value="2">2</option>
-      <option :value="3">3</option>
-    </select>
+    <input v-model="query" type="text">
+    <ul>
+      <li v-for="item in filteredItems" :key="item.value" :value="item.value" @click="onChange($event.target.value)" class="option-item">{{ item.label }}</li>
+    </ul>
     <p>{{local_val}}</p>
     <el-select v-model="local_val" filterable placeholder="Select">
       <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
@@ -29,14 +37,28 @@
     @Prop({ required: true }) private task_id!: number
 
     private prev = 1
+    private query:string = ""
+
+    get regexp() { return new RegExp(`.*${this.query}.*`, 'g') }
+
+    get filteredItems() {
+      return this.options.filter((option) => option.label.match(this.regexp))
+    }
 
     get local_val() { return this.value }
     set local_val(val) { this.input(val) }
 
-    private options: object[] = [
-      { label: "1", value: 1 },
-      { label: "2", value: 2 },
-      { label: "3", value: 3 }
+    private options: { label: string, value: number }[] = [
+      { label: "one", value: 1 },
+      { label: "two", value: 2 },
+      { label: "three", value: 3 },
+      { label: "four", value: 4 },
+      { label: "five", value: 5 },
+      { label: "six", value: 6 },
+      { label: "seven", value: 7 },
+      { label: "eight", value: 8 },
+      { label: "nine", value: 9 },
+      { label: "ten", value: 10 }
     ]
 
     private onChange(val: number): void {
@@ -48,19 +70,30 @@
         .catch(this.onError)
     }
 
-    private onSuccess(res): void { console.log("success", res); }
+    private onSuccess(res): void {
+      console.log("success", res)
+      this.$notify({
+        title: '更新成功',
+        message: '更新しました',
+        duration: 2000,
+        position: 'top-left',
+        type: 'success',
+      })
+      this.input(res.data.info.finish_type)
+    }
 
-    private onError(err): void { console.log("error", err); this.rollback() }
+    private onError(err): void {
+      console.log("error", err)
+      this.$notify({
+        title: '更新失敗',
+        message: '更新に失敗しました',
+        duration: 2000,
+        position: 'top-left',
+        type: 'error',
+      })
+    }
 
     @Emit('input')
     private input(val: number): number { return val }
-
-    @Emit('input')
-    private rollback(): number { 
-      this.prev = this.value
-      return this.prev
-    }
-
-    private prevent(event) { event.preventDefault() }
   }
 </script>
--- frontend/todo/vue.config.js
+++ frontend/todo/vue.config.js
@@ -1,18 +1,7 @@
 module.exports = {
   devServer: {
     watchOptions: {
-      poll: 5000
+      poll: 3000
     }
   },
-  chainWebpack: config => {
-    const oneOfsMap = config.module.rule('scss').oneOfs.store;
-    oneOfsMap.forEach(item => {
-      item.use('sass-resources-loader')
-          .loader('sass-resources-loader')
-          .options({ resources: ['./src/assets/scss/colors.scss',
-                                 './src/assets/scss/components.scss',
-                                 './src/assets/scss/style.scss'] })
-          .end();
-    });
-  }
 }
