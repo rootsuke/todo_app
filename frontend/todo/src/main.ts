924edd1b7e15cf20bc40379b2e5bff6ad0d7a7a8
rootsuke <rootsuke@gmail.com>
2020-05-02 21:31:02 +0900

    add icons

--- frontend/todo/src/main.ts
+++ frontend/todo/src/main.ts
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import Base from './components/base.vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import './assets/fontawesome/css/fontawesome.css';
+import './assets/fontawesome/css/regular.css';
+import './assets/fontawesome/css/solid.css';
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false;
